44c0e3224c4a44651a26d7b6cef55b6d4ca57041
rakeshsabbi <rakeshsabbisetty@gmail.com>
2022-12-06 21:32:10 +0530

    updated

---
+++ app/src/main/java/com/example/ebridge/HomeAdminActivity.java
@@ -0,0 +1,67 @@
+package com.example.ebridge;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeAdminActivity extends CommonAuth {
+
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+//    private Button btnLogout;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        if(loggedUser == null){
+
+            Intent l = new Intent(HomeAdminActivity.this, LoginActivity.class);
+            startActivity(l);
+        }
+
+        setContentView(R.layout.activity_admin_home);
+
+        drawerLayout = findViewById(R.id.id_main_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        navBars(3,HomeAdminActivity.this);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+
+    public void onStart() {
+
+        super.onStart();
+
+    }
+
+}
