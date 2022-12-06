966821f2c0a7a7391efbb26cb01c41108212e0fc
pvemul <pvemul@ilstu.edu>
2022-12-06 10:36:28 -0600

    updated

---
+++ app/src/test/java/com/example/ebridge/Model/FeedbackTest.java
@@ -0,0 +1,48 @@
+package com.example.ebridge.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FeedbackTest {
+
+    @Test
+    void getMessage() {
+    }
+
+    @Test
+    void setMessage() {
+    }
+
+    @Test
+    void getStudent() {
+    }
+
+    @Test
+    void setStudent() {
+    }
+
+    @Test
+    void getDateTime() {
+    }
+
+    @Test
+    void setDateTime() {
+    }
+
+    @Test
+    void getStatus() {
+    }
+
+    @Test
+    void setStatus() {
+    }
+
+    @Test
+    void getFeedback() {
+    }
+
+    @Test
+    void setFeedback() {
+    }
+}
---
+++ app/src/test/java/com/example/ebridge/Model/PostTest.java
@@ -0,0 +1,32 @@
+package com.example.ebridge.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PostTest {
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void getDetails() {
+    }
+
+    @Test
+    void setDetails() {
+    }
+
+    @Test
+    void getImg() {
+    }
+
+    @Test
+    void setImg() {
+    }
+}
